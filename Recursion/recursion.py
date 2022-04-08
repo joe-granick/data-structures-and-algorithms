d40537189077c668fb84b94ec1d86aba2dbf4a38
jgranick <jmgranick@gmail.com>
2022-04-08 15:28:30 -0400

    recursion problem 2: evens (Wengrow 2020, pg 181)

--- Recursion/recursion.py
+++ Recursion/recursion.py
@@ -5,3 +5,13 @@ def char_count(array, count = 0):
         count+=len(array[0])
         return char_count(array[1:], count)
 print(char_count(["ab", "c", "def", "ghij"]))
+
+def evens(nums, even_nums = []):
+    if len(nums) == 0:
+        return even_nums
+    else:
+        check_num = nums[0]
+        if check_num%2 ==0:
+            even_nums.append(check_num)
+        return evens(nums[1:], even_nums)
+print(evens([1,2,3,4,5,6,7,8,9,10]))
