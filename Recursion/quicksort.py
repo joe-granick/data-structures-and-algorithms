80e2c1c2da3c8455eb74523796f08e0358026bda
jgranick <jmgranick@gmail.com>
2022-04-13 12:07:53 -0400

    quicksort pivot created (Wengrow 2020, pg 203)

--- Recursion/quicksort.py
+++ Recursion/quicksort.py
@@ -1,29 +1,35 @@
 def quicksort(array):
-    def pivot(array):
-        pivot = len(array)-1
-        left_index = 0
-        right_index = pivot - 1
-        while array[left_index] < array[pivot] and left_index < len(array)-1:
-            left_index+=1
-        while array[right_index] > array[pivot] and right_index >0:
-            right_index-=1
-        if right_index == left_index:
-            move_val = right_index
-            pivot_val = array[pivot]
-            array[right_index] = pivot_val
-            array[pivot] = move_val
-            pivot = right_index
-            left_array = array[:pivot]
-            right_array = array[pivot+1:]
-            return[left_array, pivot, right_array]
-    if len(array) == 1:
-       return array[0]
+    pass
+def pivot(array, left_index = 0, pivot_index = None, right_index = None):
+    if pivot_index is None:
+        pivot_index = len(array)-1
+    if right_index is None:
+        right_index = pivot_index-1
+
+    if left_index == right_index:
+        array[right_index], array[pivot_index] = array[pivot_index], array[right_index]#swap pivot and right inde
+        return array
+    elif array[left_index] <= array[pivot_index]:
+        return pivot(array, left_index+1, pivot_index, right_index)
+    elif array[right_index] < array[pivot_index]:
+        array[right_index], array[pivot_index] = array[pivot_index], array[right_index]
+        return pivot(array, left_index+1, pivot_index, right_index-1)
     else:
-        partitioned = pivot(array)
-        left_array, pivot, right_array = partitioned[0], partitioned[1], partitioned[2]
-        return quicksort(left_array) + pivot + quicksort(right_array)
+        return pivot(array, left_index, pivot_index, right_index-1)
+
+
+
 
     
 
+    # if len(array) == 1:
+    #    return array[0]
+    # else:
+    #     partitioned = pivot(array)
+    #     left_array, pivot, right_array = partitioned[0], partitioned[1], partitioned[2]
+    #     return quicksort(left_array) + pivot + quicksort(right_array)
 
-print(quicksort([0, 1, 2, 3, 6, 5]))
+    
+print(pivot([0, 1, 2, 3, 6, 5]))
+
+#print(quicksort([0, 1, 2, 3, 6, 5]))
