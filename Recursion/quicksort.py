e4e74fb81e912ac5ae6fcccff3e6416ffd9e29fb
jgranick <jmgranick@gmail.com>
2022-04-14 18:01:02 -0400

    Quicksort complete (Wengrow, pg 212)

--- Recursion/quicksort.py
+++ Recursion/quicksort.py
@@ -1,7 +1,5 @@
-def quicksort(array):
-    pass
 def pivot(array, left_index = 0, pivot_index = None, right_index = None):
-    print(array)
+    #print(array)
     if pivot_index is None:
         pivot_index = len(array)-1
     if right_index is None:
@@ -11,13 +9,25 @@ def pivot(array, left_index = 0, pivot_index = None, right_index = None):
     while array[right_index] > array[pivot_index] and right_index > 0:
         right_index-=1
     if left_index >= right_index:
-        print("swap left and pivot")
+        #print("swap left and pivot")
         array[pivot_index], array[left_index] = array[left_index], array[pivot_index]
+        return [array, left_index]
+    else:
+        #print("swap right and left")
+        array[right_index], array[left_index] = array[left_index], array[right_index]
+        print(array)
+        return pivot(array, left_index, pivot_index, right_index)
+
+def quicksort(array):
+    if len(array) <= 1:
         return array
     else:
-        print("swap right and left")
-        array[right_index], array[left_index] = array[left_index], array[right_index]
-        return pivot(array, left_index, pivot_index, right_index)
+        array_pivot = pivot(array)
+        pivot_index = array_pivot[1]
+        pivot_val = array_pivot[0][pivot_index]
+        left_array = array_pivot[0][:pivot_index]
+        right_array = array[pivot_index+1:]
+        return quicksort(left_array) + [pivot_val] + quicksort(right_array)
     
   
     
@@ -37,4 +47,5 @@ def pivot(array, left_index = 0, pivot_index = None, right_index = None):
     
 print(pivot([0, 5, 2, 1, 6, 3]))
 
+print(quicksort([0, 5, 2, 1, 6, 3]))
 #print(quicksort([0, 1, 2, 3, 6, 5]))
