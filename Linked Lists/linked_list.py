b942e7a9c05038c40d9d86cf396fba5e1b692663
jgranick <jmgranick@gmail.com>
2022-04-17 19:52:51 -0400

    Python linked list delete, insert, search, read, print. Problems 1,3 (Wengrow, pg 245)

--- Linked Lists/linked_list.py
+++ Linked Lists/linked_list.py
@@ -1,11 +1,127 @@
+from pickle import LIST
+
+
 class node:
     def __init__(self, node_val = None):   
         self.node_val = node_val
         self.next_node = None
+        self.previous_node = None
 
 class linked_list:
     def __init__(self):
         self.first_node = None
+    def read(self, index):
+        current_node = self.first_node
+        current_index = 0
+        while current_index < index:
+            current_node = current_node.next_node
+            current_index+=1
+        return current_node.node_val
+    def search(self, val):
+        current_node = self.first_node
+        index = 0
+        while current_node:
+            if current_node.node_val == val:
+                return index
+            current_node = current_node.next_node
+            index+=1
+        return None
+    def insert(self, val, index):
+        current_node = self.first_node
+        new_node = node(val)
+        current_index = 0
+        if index == 0:
+            new_node.next_node = self.first_node
+            self.first_node = new_node
+            #self.first_node.next_node = current_node  
+        else:
+            while current_index < index-1:
+                current_node = current_node.next_node
+                current_index+=1
+            next_next_node = current_node.next_node
+            current_node.next_node = new_node
+            current_node.next_node.next_node = next_next_node
+    def delete(self, index):
+        current_node = self.first_node
+        current_index = 0
+        if index == 0:
+            self.first_node = self.first_node.next_node
+        else:
+            while current_index < index-1:
+                current_node = current_node.next_node
+                current_index +=1
+            new_next_node = current_node.next_node.next_node
+            current_node.next_node = new_next_node
+    def print_list(self):
+        current_node = self.first_node
+        node_index = 0
+        while current_node:
+            val = current_node.node_val
+            print("Node ", node_index ,":", val)
+            node_index+=1
+            current_node = current_node.next_node
+    def last_node(self):
+        current_node = self.first_node
+        while  current_node.next_node:
+            current_node = current_node.next_node
+        return current_node
+
+class double_linked_list:
+    def __init__(self):
+        self.first_node = None
+        self.last_node = None
+    def read(self, index):
+        current_node = self.first_node
+        current_index = 0
+        while current_index < index:
+            current_node = current_node.next_node
+            current_index+=1
+        return current_node.node_val
+    def search(self, val):
+        current_node = self.first_node
+        index = 0
+        while current_node:
+            if current_node.node_val == val:
+                return index
+            current_node = current_node.next_node
+            index+=1
+        return None
+    def insert(self, val, index):
+        current_node = self.first_node
+        new_node = node(val)
+        current_index = 0
+        if index == 0:
+            new_node.next_node = self.first_node
+            self.first_node = new_node
+            #self.first_node.next_node = current_node  
+        else:
+            while current_index < index-1:
+                current_node = current_node.next_node
+                current_index+=1
+            next_next_node = current_node.next_node
+            current_node.next_node = new_node
+            current_node.next_node.next_node = next_next_node
+    def delete(self, index):
+        current_node = self.first_node
+        current_index = 0
+        if index == 0:
+            self.first_node = self.first_node.next_node
+        else:
+            while current_index < index-1:
+                current_node = current_node.next_node
+                current_index +=1
+            new_next_node = current_node.next_node.next_node
+            current_node.next_node = new_next_node
+    def print_list(self):
+        current_node = self.first_node
+        node_index = 0
+        while current_node:
+            val = current_node.node_val
+            print("Node ", node_index ,":", val)
+            node_index+=1
+            current_node = current_node.next_node
+    
+
 
 
 list_1 = linked_list()
@@ -20,6 +136,38 @@ node_2.next_node = node_3
 print(list_1.first_node.node_val)
 print(list_1.first_node.next_node.node_val)
 print(list_1.first_node.next_node.next_node.node_val)
+print(list_1.read(0))
+
+
+print(list_1.search('this is first_node'))
+print(list_1.search('this is second node'))
+print(list_1.search('this is third node'))
+print(list_1.search('this is first node'))
+
+list_1.print_list()
+print("Insert index 0")
+list_1.insert("this is new first node", 0)
+list_1.print_list()
+print("Insert index 1")
+list_1.insert("this is new second node", 1)
+list_1.print_list()
+print("Insert index 5")
+list_1.insert("this is new last node", 5)
+list_1.print_list()
+
+#list_1.insert("this is extra last node", 10)
+#print(list_1.read(6))
+
+list_1.print_list()
+print("Delete index 0")
+list_1.delete(0)
+list_1.print_list()
+print("Delete index 4")
+list_1.delete(4)
+#print(list_1.read(4))
+list_1.print_list()
+print(list_1.last_node().node_val)
+
 
 
 
