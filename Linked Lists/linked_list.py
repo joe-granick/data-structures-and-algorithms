306b50db46abb97f147ed0e25c9b3daebcb3f92b
jgranick <jmgranick@gmail.com>
2022-04-19 17:28:28 -0400

    Python double linked list insert_at_end implementation (Wengrow, pg 241)

--- Linked Lists/linked_list.py
+++ Linked Lists/linked_list.py
@@ -26,14 +26,13 @@ class linked_list:
             current_node = current_node.next_node
             index+=1
         return None
-    def insert(self, val, index):
+    def insert(self, val, index=0):
         current_node = self.first_node
         new_node = node(val)
         current_index = 0
         if index == 0:
             new_node.next_node = self.first_node
             self.first_node = new_node
-            #self.first_node.next_node = current_node  
         else:
             while current_index < index-1:
                 current_node = current_node.next_node
@@ -41,7 +40,7 @@ class linked_list:
             next_next_node = current_node.next_node
             current_node.next_node = new_node
             current_node.next_node.next_node = next_next_node
-    def delete(self, index):
+    def delete(self, index=0):
         current_node = self.first_node
         current_index = 0
         if index == 0:
@@ -65,11 +64,18 @@ class linked_list:
         while  current_node.next_node:
             current_node = current_node.next_node
         return current_node
+    def reverse_list(self):
+        current_node = self.first_node
+        insert_node = current_node.next_node
+        current_node.next_node.delete()
+        self.insert(insert_node.node_val, 0)
+        pass
 
 class double_linked_list:
     def __init__(self):
         self.first_node = None
         self.last_node = None
+    
     def read(self, index):
         current_node = self.first_node
         current_index = 0
@@ -77,6 +83,7 @@ class double_linked_list:
             current_node = current_node.next_node
             current_index+=1
         return current_node.node_val
+    
     def search(self, val):
         current_node = self.first_node
         index = 0
@@ -86,6 +93,7 @@ class double_linked_list:
             current_node = current_node.next_node
             index+=1
         return None
+    
     def insert(self, val, index):
         current_node = self.first_node
         new_node = node(val)
@@ -101,6 +109,17 @@ class double_linked_list:
             next_next_node = current_node.next_node
             current_node.next_node = new_node
             current_node.next_node.next_node = next_next_node
+    
+    def insert_at_end(self, val):
+        new_node = node(val)
+        if not self.first_node:
+            self.first_node = new_node
+            self.last_node = new_node
+        else:
+            new_node.previous_node = self.last_node
+            self.last_node.next_node = new_node
+            self.last_node = new_node
+
     def delete(self, index):
         current_node = self.first_node
         current_index = 0
@@ -112,6 +131,7 @@ class double_linked_list:
                 current_index +=1
             new_next_node = current_node.next_node.next_node
             current_node.next_node = new_next_node
+    
     def print_list(self):
         current_node = self.first_node
         node_index = 0
@@ -167,6 +187,27 @@ list_1.delete(4)
 #print(list_1.read(4))
 list_1.print_list()
 print(list_1.last_node().node_val)
+
+list_2 = double_linked_list()
+
+list_2.insert_at_end("This is first value in double linked list")
+list_2.print_list()
+
+list_2.insert_at_end("This is second value in double linked list")
+list_2.print_list()
+
+list_2.insert_at_end("This is third value in double linked list")
+list_2.print_list()
+
+#print(list_2.read(0))
+print(list_2.first_node.node_val)
+print(list_2.last_node.node_val)
+print(list_2.last_node.previous_node.node_val)
+
+
+
+
+
 
 
 
