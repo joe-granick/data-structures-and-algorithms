18fc740eff7ca44a737207a18c0b3a725faca88c
jgranick <jmgranick@gmail.com>
2022-04-22 19:27:08 -0400

    Create binary search tree insert function (Wengrow, pg 258)

--- Trees/tree.py
+++ Trees/tree.py
@@ -8,6 +8,18 @@ def tree_search(search_val, tree_node):
     if search_val == tree_node.val or tree_node is None:
         return tree_node
     elif search_val < tree_node.val:
-        tree_search(search_val, tree_node.left_branch)
+        return tree_search(search_val, tree_node.left_branch)
     else:
-        tree_search(search_val, tree_node.right_branch)
+        return tree_search(search_val, tree_node.right_branch)
+
+def tree_insert(tree_node, insert_val):
+    if insert_val < tree_node.val:
+        if  tree_node.left_branch is None:
+            tree_node.left_branch = tree_node(insert_val)
+        else:
+            tree_insert(tree_node.left_branch, insert_val)
+    elif insert_val > tree_node.val:
+        if  tree_node.right_branch is None:
+            tree_node.right_branch = tree_node(insert_val)
+        else:
+            tree_insert(tree_node.right_branch, insert_val)
