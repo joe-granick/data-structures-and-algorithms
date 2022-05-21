698fc72c784e592bd0191db16e18f108f3754c79
jgranick <jmgranick@gmail.com>
2022-05-21 19:37:56 -0400

    trie class outlined

---
+++ tries/trie.py
@@ -0,0 +1,20 @@
+class trieNode:
+    def __init__(self):
+        children = {}
+
+class trie:
+
+    def __init__(self):
+        root = trieNode()
+
+    def search(self, input):
+        pass
+
+    def insert(self, prefix):
+        pass
+
+    def collectAllWords(self, node = None, word = "", words = []):
+        pass
+
+    def autocomplete(self, prefix):
+        pass
