abaf5386c17391680583f25850627db3e6efcdd2
jgranick <jmgranick@gmail.com>
2022-05-21 20:02:58 -0400

    trie and trie node classes created

--- tries/trie.py
+++ tries/trie.py
@@ -3,18 +3,42 @@ class trieNode:
         children = {}
 
 class trie:
-
     def __init__(self):
         root = trieNode()
 
     def search(self, input):
-        pass
+        currentNode = self.root
+        for char in input:
+            if not currentNode.children.get(char):
+                currentNode = currentNode.children[char]
+            else:
+                return None
+            return currentNode
 
     def insert(self, prefix):
-        pass
-
+        currentNode = self.root
+        for char in prefix:
+            if currentNode.children.get(char):
+                currentNode = currentNode.children[char]
+            else:
+                newNode = trieNode()
+                currentNode.children[char] = newNode
+        currentNode.children["*"] = None
+            
     def collectAllWords(self, node = None, word = "", words = []):
-        pass
+        currentNode = node or self.root
+
+        for key, child in currentNode.children.items():
+            if key == "*":
+                words.append(word)
+            else:
+                self.collectAllWords(child, word + key, words)
+
 
     def autocomplete(self, prefix):
-        pass
+        startNode = self.search(prefix)
+        if not startNode:
+            return None
+        else:
+            return self.collectAllWords(startNode, prefix)
+
